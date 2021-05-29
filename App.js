16ac5428957c71728f1a7c54a0f42eec4458410c
JaweriyaArif2002 <jaweriyaurooj803@gmail.com>
2021-05-28 23:32:11 -0700

    react-native-vector-icons

---
+++ App.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { View,Text } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons'
+function App(){
+  return(
+    <View>
+      <Icon name="md-menu" size={50}/>
+      <Text>
+        APp
+      </Text>
+    </View>
+  )
+}
+
+
+export default App;
